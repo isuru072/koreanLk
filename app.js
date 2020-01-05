f2f27898c2ea8883c4e58965fa7e0355c205f338
thushanisuru <isuru072@gmail.com>
2020-01-05 21:12:36 +0530

    change port

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var PORT = process.env.PORT || 5000;
 
 var http = require('http');
 
@@ -5,4 +6,4 @@ var server = http.createServer(function(req, res) {
 res.writeHead(200);
 res.end('Hi everybody!');
 });
-server.listen(3000)|| process.env.server ;
+server.listen(PORT);
