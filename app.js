6d9542ca450daf7da85659c52c4ffdee4691b4a4
thushanisuru <isuru072@gmail.com>
2020-01-05 21:06:56 +0530

    add serive or local

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
 res.end('Hi everybody!');
 });
-server.listen(3000);
+server.listen(3000)|| process.env.server ;
